e7c7b3f2b482749ca656594a020afae5d57bc366
DidinSalahudin <didinsalahudin@gmail.com>
2019-10-04 17:02:45 +0700

    Updates

---
+++ precache-manifest.f52e5da1b31cb7fba1f13fc3779dc234.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3355c10e1c235ceddaf6fa4752c183b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "902136ea990612dfdcdd",
+    "url": "/portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "039a47dc00bd8cb38094",
+    "url": "/portfolio/static/js/2.f9e28181.chunk.js"
+  },
+  {
+    "revision": "902136ea990612dfdcdd",
+    "url": "/portfolio/static/js/main.6e62cc09.chunk.js"
+  },
+  {
+    "revision": "c5306228cc83f6f6271c",
+    "url": "/portfolio/static/js/runtime-main.7676bd13.js"
+  }
+]);
