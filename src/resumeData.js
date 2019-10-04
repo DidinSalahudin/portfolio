65f93723673518ed08833411ccdad9e377bfc877
DidinSalahudin <didinsalahudin@gmail.com>
2019-10-04 17:01:18 +0700

    Update File

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Salahudin",
+    "role": "Full Stack Developer",
+    "linkedinId":"DidinSalahudin",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of full stack development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url": "https://www.linkedin.com/in/didin-salahudin-baa39b135/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/didinsalahudin",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/didinsalahudin",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme": "I currently work as a Full Stack Developer. I'm a Full Stack Web developer who taught himself, currently diving deeper into the Front End Developer. I believe that to be successful in life, a person must be obsessed with their dreams and continue to work towards them.",
+    "address":"Jakarta- Indonesia",
+    "website":"https://didinsalahudin.github.io/portfolio",
+    "education":[
+      {
+        "UniversityName": "STMIK Nusa Mandiri",
+        "specialization": "Information System",
+        "MonthOfPassing": "Nov",
+        "YearOfPassing": "2020",
+        "Achievements": "Some Achievements"        
+      },
+      {
+        "UniversityName": "Bina Sarana Informatika University",
+        "specialization": "Information System",
+        "MonthOfPassing": "November",
+        "YearOfPassing": "2019",
+        "Achievements": "Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"PT Reventon Mitra Pratama (cekpremi.com)",
+        "specialization":"Full Stack Developer",
+        "MonthOfLeaving":"2017 ",
+        "YearOfLeaving": "- Currently",
+        "Achievements":"Some Achievements"
+      }
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
